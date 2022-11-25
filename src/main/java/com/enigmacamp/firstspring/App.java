43d31205082a69f76b85c4cb8627a9c562ea61de
edwardsuwirya <edward_suwirya@yahoo.com>
2022-11-25 09:44:47 +0700

    Spring ApplicationContext.xml

--- src/main/java/com/enigmacamp/firstspring/App.java
+++ src/main/java/com/enigmacamp/firstspring/App.java
@@ -3,17 +3,26 @@ package com.enigmacamp.firstspring;
 import com.enigmacamp.firstspring.entity.Course;
 import com.enigmacamp.firstspring.service.CourseService;
 import com.enigmacamp.firstspring.service.ICourseService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
 
-        //Ngga banget nih....
-        //1. CourseService adalah dependensi dari class App
-//        Ini bukan tanggung jawab dari class Main untuk membuat object dep
-//        (CourseService)
-        ICourseService courseService = new CourseService();
+
+        // Penerapan IoC, tanggung jawab pembentukan obj CourseService diserahkan
+//        ke spring framework
+//        Deklarasi object ada 2 cara
+//        1. XML
+//        2. Java
+//        Object dependensi yang didaftarkan di Spring IoC Container disebuat bean
+//        Untuk menggunakan spring bean di class yang membutuhkan, kita gunakan
+//        Spring Context
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        ICourseService courseService = (ICourseService) context.getBean("courseService");
 
         Course springCourse = new Course();
         springCourse.setCourseId("123");
--- src/main/java/com/enigmacamp/firstspring/service/ICourseService.java
+++ src/main/java/com/enigmacamp/firstspring/service/ICourseService.java
@@ -4,6 +4,9 @@ import com.enigmacamp.firstspring.entity.Course;
 
 import java.util.List;
 
+// Penggunaan interface akan membuat kebutuhan dep menjadi loose coupling
+// Obj Dep akan menjadi lebih fleksibel dalam implementasinya
+// Sesuai dengan prinsip Open For Extension, Close For Modification
 public interface ICourseService {
     Course create(Course course);
 
